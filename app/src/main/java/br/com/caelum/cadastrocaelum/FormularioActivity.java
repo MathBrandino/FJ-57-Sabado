ea9ec2e1f48f34fd632a40420bf55a77509c1e84
Matheus Brandino <m.v.brandino@gmail.com>
2018-05-19 15:33:13 -0300

    ex 3.1 criando formulario com material design

---
+++ app/src/main/java/br/com/caelum/cadastrocaelum/FormularioActivity.java
@@ -0,0 +1,15 @@
+package br.com.caelum.cadastrocaelum;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+    }
+}
