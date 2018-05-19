8a3211f3d8907a84d36d91e42c8cbda6f7879b52
Matheus Brandino <m.v.brandino@gmail.com>
2018-05-19 14:40:59 -0300

    ex 2.3 - criando listeners na lista e toast

--- app/src/main/java/br/com/caelum/cadastrocaelum/ListaAlunosActivity.java
+++ app/src/main/java/br/com/caelum/cadastrocaelum/ListaAlunosActivity.java
@@ -1,10 +1,12 @@
 package br.com.caelum.cadastrocaelum;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -15,13 +17,38 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         String[] alunos = {"Gabriel", "Luiz", "Dilan", "Janaina"};
 
-        ListView lista = findViewById(R.id.lista);
+        final ListView lista = findViewById(R.id.lista);
 
 
         ArrayAdapter<String> adapter =
                 new ArrayAdapter(this,android.R.layout.simple_list_item_1,alunos);
 
         lista.setAdapter(adapter);
+
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+
+                String aluno = (String) lista.getItemAtPosition(posicao);
+
+                Toast.makeText(ListaAlunosActivity.this, aluno, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+
+
+                Toast.makeText(ListaAlunosActivity.this, "A posição é: " + posicao, Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
+
 
 
 
