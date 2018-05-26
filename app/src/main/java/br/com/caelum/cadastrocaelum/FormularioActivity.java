9a4ac36e9b2a9713450b8b764131e8a45f9e0aa8
Matheus Brandino <m.v.brandino@gmail.com>
2018-05-26 10:56:31 -0300

    criando o menu no formulario - ex 3.9

--- app/src/main/java/br/com/caelum/cadastrocaelum/FormularioActivity.java
+++ app/src/main/java/br/com/caelum/cadastrocaelum/FormularioActivity.java
@@ -1,11 +1,11 @@
 package br.com.caelum.cadastrocaelum;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -15,17 +15,34 @@ public class FormularioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
 
 
-        Button salvar = findViewById(R.id.formulario_salvar);
+    }
 
-        salvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                finish();
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.formulario_menu, menu);
+
+        return true;
 
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.formulario_menu_salvar:
+                finish();
+
+                break;
+
+
+        }
+
+        return true;
+    }
 }
