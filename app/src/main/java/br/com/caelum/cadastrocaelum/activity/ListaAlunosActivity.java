68cd5cf160cc9164ad0f3853dde1fafbd1857f20
Matheus Brandino <m.v.brandino@gmail.com>
2018-06-09 14:57:44 -0300

    criando aluno adapter - ex 10.2

--- app/src/main/java/br/com/caelum/cadastrocaelum/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/caelum/cadastrocaelum/activity/ListaAlunosActivity.java
@@ -15,16 +15,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import br.com.caelum.cadastrocaelum.R;
 import br.com.caelum.cadastrocaelum.adapter.AlunoAdapter;
+import br.com.caelum.cadastrocaelum.converter.AlunoConverter;
 import br.com.caelum.cadastrocaelum.dao.AlunoDAO;
 import br.com.caelum.cadastrocaelum.modelo.Aluno;
 
@@ -210,5 +212,33 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.lista_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()){
+            case R.id.lista_envia_notas:
+
+                AlunoDAO alunoDAO = new AlunoDAO(this);
+                List<Aluno> alunos = alunoDAO.getAlunos();
+                alunoDAO.close();
+
+                String json = new AlunoConverter().toJSON(alunos);
+
+                Toast.makeText(this, json, Toast.LENGTH_SHORT).show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
